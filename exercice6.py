fd105ccfe393c0c15be0ee24165af9023c4c2500
Selamet53 <selamet.zamanoglu@polymtl.ca>
2021-09-21 18:15:28 -0400

    Commit 1
    commit 1

--- exercice6.py
+++ exercice6.py
@@ -8,35 +8,36 @@ def trouverAngle(nombreComplexe):
 
 def trouverModule(nombreComplexe):
     # TODO: Calculer le module du nombre complexe et l'assigner dans "module"
-    module =
+    module = math.sqrt(nombreComplexe.real**2 + nombreComplexe.imag**2)
 
     return module
 
 
 
-def effectuerRotation(nombreComplexe, angle_rotation, trouverModule):
+def effectuerRotation(nombreComplexe, rotation, trouverModule):
 
     module = trouverModule(nombreComplexe)
     angle = trouverAngle(nombreComplexe)
 
     # TODO: Afficher le module et l'angle du nombre complexe (3 decimales de précision)
-
+    print(round(module,3),round(angle,3))
 
     # TODO: Calculer le nouveau nombre complexe après rotation, assigner le nouveau nombre complexe à la variable 'resultat'
-
-    resultat =
+    i=complex(0,1)
+    resultat =nombreComplexe*(math.cos(rotation)+math.sin(rotation)*i)
 
     nouveauModule = trouverModule(resultat)
     nouvelAngle = trouverAngle(resultat)
 
     # TODO : Afficher le nouveau module et le nouvel angle du nombre complexe après rotation (3 decimales de précision)
-
+    print(round(nouveauModule,3),round(nouvelAngle,3))
     return resultat
 
 
 def dessiner(number, label):
+    ax = plt.subplot(projection='polar')
     if number != None:
-        plt.polar([0, math.radians(trouverAngle(number))], [0, trouverModule(number)], marker='o', label=label)
+        ax.plot([0, math.radians(trouverAngle(number))], [0, trouverModule(number)], marker='o', label=label)
 
 if __name__ == '__main__':
     nombre = complex(input("Veuillez entrer un nombre complexe de votre choix sous la forme a+bj (exemple: 1+2j): "))
